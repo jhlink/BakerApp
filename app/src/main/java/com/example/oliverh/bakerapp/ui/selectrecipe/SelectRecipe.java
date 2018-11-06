a62f2dfe218111f9a75691d395f4051d3a1a1730
James Kim <jhlink@users.noreply.github.com>
2018-11-06 13:09:29 -0500

    feat: Plant debugTree in SelectREcipe

--- app/src/main/java/com/example/oliverh/bakerapp/ui/selectrecipe/SelectRecipe.java
+++ app/src/main/java/com/example/oliverh/bakerapp/ui/selectrecipe/SelectRecipe.java
@@ -1,10 +1,13 @@
 package com.example.oliverh.bakerapp.ui.selectrecipe;
 
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.oliverh.bakerapp.R;
 import com.example.oliverh.bakerapp.ui.selectrecipe.SelectRecipeFragment;
+
+import timber.log.Timber;
 
 public class SelectRecipe extends AppCompatActivity {
 
@@ -17,5 +20,7 @@ public class SelectRecipe extends AppCompatActivity {
                     .replace(R.id.container, SelectRecipeFragment.newInstance())
                     .commitNow();
         }
+
+        Timber.plant(new Timber.DebugTree());
     }
 }
