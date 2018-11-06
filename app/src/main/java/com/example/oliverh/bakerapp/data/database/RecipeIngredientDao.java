6cc3aa2d3e6b03616bab8e07f8b82b02baa9c480
James Kim <jhlink@users.noreply.github.com>
2018-11-06 18:21:06 -0500

    feat: Implement RecipeIngredientDao

---
+++ app/src/main/java/com/example/oliverh/bakerapp/data/database/RecipeIngredientDao.java
@@ -0,0 +1,18 @@
+package com.example.oliverh.bakerapp.data.database;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface RecipeIngredientDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertRecipeIngredients(RecipeIngredient... recipeIngredients);
+
+    @Query("SELECT * FROM RecipeIngredient WHERE recipe_id =:recipeId")
+    LiveData<List<RecipeIngredient>> getRecipeIngredientsById(int recipeId);
+}
