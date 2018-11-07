48ff6637bd6dd7168dfa0d9b798546ca39d75e16
James Kim <jhlink@users.noreply.github.com>
2018-11-07 18:53:38 -0500

    feat: Create RepositoryResponse class

---
+++ app/src/main/java/com/example/oliverh/bakerapp/data/network/RepositoryResponse.java
@@ -0,0 +1,35 @@
+package com.example.oliverh.bakerapp.data.network;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepositoryResponse<T> {
+    private List<T> listOfData;
+    private Throwable error;
+
+    public RepositoryResponse(List<T> data) {
+        this.listOfData = data;
+        this.error = null;
+    }
+
+    public RepositoryResponse(Throwable err) {
+        this.listOfData = new ArrayList<>();
+        this.error = err;
+    }
+
+    public List<T> getListOfData() {
+        return listOfData;
+    }
+
+    public void setListOfData(List<T> listOfData) {
+        this.listOfData = listOfData;
+    }
+
+    public Throwable getError() {
+        return error;
+    }
+
+    public void setError(Throwable error) {
+        this.error = error;
+    }
+}
