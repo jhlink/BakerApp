0f403c50aab9d4ac2b5dde2488338e26490e9042
James Kim <jhlink@users.noreply.github.com>
2018-11-07 13:24:27 -0500

    feat: Supply private list members for RecipeIngredients and RecipeSteps only for Moshi ( ignored by Room )

--- app/src/main/java/com/example/oliverh/bakerapp/data/database/Recipe.java
+++ app/src/main/java/com/example/oliverh/bakerapp/data/database/Recipe.java
@@ -3,7 +3,10 @@ package com.example.oliverh.bakerapp.data.database;
 
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.PrimaryKey;
+
+import java.util.ArrayList;
 
 @Entity
 public class Recipe {
@@ -19,6 +22,12 @@ public class Recipe {
 
     @ColumnInfo(name = "image")
     private String imageURL;
+
+    @Ignore
+    private ArrayList<RecipeIngredient> ingredients;
+
+    @Ignore
+    private ArrayList<RecipeStep> steps;
 
     public Recipe(int id, String recipeName, int servings, String imageURL) {
         this.id = id;
@@ -59,5 +68,19 @@ public class Recipe {
         this.imageURL = mImageURL;
     }
 
+    public ArrayList<RecipeIngredient> getIngredients() {
+        return ingredients;
+    }
 
+    public void setIngredients(ArrayList<RecipeIngredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public ArrayList<RecipeStep> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(ArrayList<RecipeStep> steps) {
+        this.steps = steps;
+    }
 }
