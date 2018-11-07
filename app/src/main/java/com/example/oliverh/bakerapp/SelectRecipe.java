180f9e3e0a48da7d10d282b5b2761beb753136a2
James Kim <jhlink@users.noreply.github.com>
2018-11-06 19:18:31 -0500

    fix: Plant Timber.DebugTree

--- app/src/main/java/com/example/oliverh/bakerapp/SelectRecipe.java
+++ app/src/main/java/com/example/oliverh/bakerapp/SelectRecipe.java
@@ -1,9 +1,14 @@
 package com.example.oliverh.bakerapp;
 
+import android.arch.persistence.room.Database;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.oliverh.bakerapp.data.RecipeRepository;
+import com.example.oliverh.bakerapp.data.database.AppDatabase;
 import com.example.oliverh.bakerapp.ui.selectrecipe.SelectRecipeFragment;
+
+import timber.log.Timber;
 
 public class SelectRecipe extends AppCompatActivity {
 
@@ -16,5 +21,7 @@ public class SelectRecipe extends AppCompatActivity {
                     .replace(R.id.container, SelectRecipeFragment.newInstance())
                     .commitNow();
         }
+
+        Timber.plant(new Timber.DebugTree());
     }
 }
