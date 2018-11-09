fd62f8d2b189e6fde9b3832c52ed72fd24348954
jhlink <jhlink@users.noreply.github.com>
2018-11-08 20:12:28 -0500

    feat: Implement SelectRecipeViewModel

--- app/src/main/java/com/example/oliverh/bakerapp/ui/selectrecipe/SelectRecipeViewModel.java
+++ app/src/main/java/com/example/oliverh/bakerapp/ui/selectrecipe/SelectRecipeViewModel.java
@@ -1,7 +1,26 @@
 package com.example.oliverh.bakerapp.ui.selectrecipe;
 
-import android.arch.lifecycle.ViewModel;
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
 
-public class SelectRecipeViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+import com.example.oliverh.bakerapp.data.RecipeRepository;
+import com.example.oliverh.bakerapp.data.database.AppDatabase;
+import com.example.oliverh.bakerapp.data.network.RepositoryResponse;
+
+public class SelectRecipeViewModel extends AndroidViewModel {
+    private final RecipeRepository mRepository;
+    private final LiveData<RepositoryResponse> recipes;
+
+    public SelectRecipeViewModel(@NonNull Application application) {
+        super(application);
+        AppDatabase appDatabase = AppDatabase.getInstance(application);
+        mRepository = RecipeRepository.getInstance(application, appDatabase);
+        recipes = mRepository.getRecipeList();
+    }
+
+    public LiveData<RepositoryResponse> getRecipes() {
+        return recipes;
+    }
 }
