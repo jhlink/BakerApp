c7aaedd0dd611a7ab3500af1c9ee222583444686
jhlink <jhlink@users.noreply.github.com>
2018-11-10 17:51:12 -0500

    fix: Correct step_id used for RecipeStep query in RecipeStepDao

--- app/src/main/java/com/example/oliverh/bakerapp/data/database/RecipeStepDao.java
+++ app/src/main/java/com/example/oliverh/bakerapp/data/database/RecipeStepDao.java
@@ -16,7 +16,7 @@ public interface RecipeStepDao {
     @Query("SELECT * FROM RecipeStep WHERE recipe_id =:recipeId")
     LiveData<List<RecipeStep>> getRecipeStepsByRecipeId(int recipeId);
 
-    @Query("SELECT * FROM RecipeStep WHERE recipe_id =:recipeId AND recipe_step_id =:stepId")
+    @Query("SELECT * FROM RecipeStep WHERE recipe_id =:recipeId AND recipe_step_index =:stepId")
     LiveData<RecipeStep> getRecipeStepByRecipeId(int recipeId, int stepId);
 
     @Query("DELETE FROM Recipe")
