146c9b7fddafaaf2e5834ddf3d0fcf8d2e139046
jhlink <jhlink@users.noreply.github.com>
2018-11-10 17:40:01 -0500

    feat: Change SimpleExoPlayerView id

--- app/src/main/java/com/example/oliverh/bakerapp/ui/viewstep/ViewRecipeStepFragment.java
+++ app/src/main/java/com/example/oliverh/bakerapp/ui/viewstep/ViewRecipeStepFragment.java
@@ -1,5 +1,6 @@
 package com.example.oliverh.bakerapp.ui.viewstep;
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,13 +9,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.oliverh.bakerapp.R;
+import com.example.oliverh.bakerapp.data.database.Recipe;
+import com.example.oliverh.bakerapp.data.database.RecipeStep;
+import com.example.oliverh.bakerapp.data.network.RepositoryResponse;
 import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import timber.log.Timber;
 
 public class ViewRecipeStepFragment extends Fragment implements ExoPlayer.EventListener {
 
@@ -23,6 +34,18 @@ public class ViewRecipeStepFragment extends Fragment implements ExoPlayer.EventL
     private ViewRecipeStepViewModel mViewModel;
     private int mRecipeId;
     private int mStepId;
+
+    @BindView(R.id.recipePlayerView)
+    SimpleExoPlayerView playerView;
+
+    @BindView(R.id.tv_recipeStepHeader)
+    TextView recipeStepHeader;
+
+    @BindView(R.id.tv_recipeStepDesc)
+    TextView recipeStepDescription;
+
+    @BindView(R.id.btn_nextStep)
+    Button nextStepButton;
 
     public static ViewRecipeStepFragment newInstance(int recipeId, int stepId ) {
         ViewRecipeStepFragment fragment = new ViewRecipeStepFragment();
@@ -45,6 +68,21 @@ public class ViewRecipeStepFragment extends Fragment implements ExoPlayer.EventL
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.view_recipe_step_activity, container, false);
+
+        ButterKnife.bind(this, view);
+
+        mViewModel.getRecipeStep().observe(this, new Observer<RepositoryResponse>() {
+            @Override
+            public void onChanged(@Nullable RepositoryResponse repositoryResponse) {
+                if (repositoryResponse.getError() != null) {
+                    Timber.d(repositoryResponse.getError());
+                    return;
+                }
+                RecipeStep recipeStep = (RecipeStep) repositoryResponse.getObject();
+                recipeStepHeader.setText(String.format("Step %d", recipeStep.getStepIndex() + 1));
+                recipeStepDescription.setText(recipeStep.getDescription());
+            }
+        });
 
         return view;
     }
