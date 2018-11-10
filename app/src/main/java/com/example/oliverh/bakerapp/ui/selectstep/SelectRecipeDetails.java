99c1574dc72bf51ec199fc725b89169da508194d
jhlink <jhlink@users.noreply.github.com>
2018-11-10 10:28:34 -0500

    feat: Implement SelectRecipeDetails activity

---
+++ app/src/main/java/com/example/oliverh/bakerapp/ui/selectstep/SelectRecipeDetails.java
@@ -0,0 +1,43 @@
+package com.example.oliverh.bakerapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.oliverh.bakerapp.ui.selectstep.SelectRecipeDetailsFragment;
+
+import timber.log.Timber;
+
+public class SelectRecipeDetails extends AppCompatActivity {
+
+    public static final String RECIPE_DETAILS_FRAGMENT_TAG = "RECIPE_DETAILS_FRAGMENT";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_recipe_activity);
+
+        int recipeId = getIntent().getIntExtra(getString(R.string.BUNDLE_RECIPE_ID), -1);
+
+        if (savedInstanceState == null) {
+
+            // TODO: Properly handle this.
+
+            SelectRecipeDetailsFragment fragment = (SelectRecipeDetailsFragment) getSupportFragmentManager().findFragmentByTag(RECIPE_DETAILS_FRAGMENT_TAG);
+            if (fragment == null) {
+                Timber.d("Create fragment.");
+
+                fragment = SelectRecipeDetailsFragment.newInstance(1);
+            } else {
+                Timber.d("Found fragment.");
+            }
+
+            fragment.setRecipeId(recipeId);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.recipe_collection_container,
+                            fragment,
+                            RECIPE_DETAILS_FRAGMENT_TAG)
+                    .commitNow();
+        }
+    }
+}
