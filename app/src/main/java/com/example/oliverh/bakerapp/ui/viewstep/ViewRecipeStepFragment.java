fcbf2e73bd0433c17665245ae88d8d88125e42e5
jhlink <jhlink@users.noreply.github.com>
2018-11-10 15:25:25 -0500

    feat: Implement basic ViewRecipeStepFragment with ExoPlayer.EventListener

---
+++ app/src/main/java/com/example/oliverh/bakerapp/ui/viewstep/ViewRecipeStepFragment.java
@@ -0,0 +1,71 @@
+package com.example.oliverh.bakerapp.ui.viewstep;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.oliverh.bakerapp.R;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+
+public class ViewRecipeStepFragment extends Fragment implements ExoPlayer.EventListener {
+
+    private ViewRecipeStepViewModel mViewModel;
+
+    public static ViewRecipeStepFragment newInstance() {
+        return new ViewRecipeStepFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mViewModel = ViewModelProviders.of(this).get(ViewRecipeStepViewModel.class);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.view_recipe_step_activity, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity() {
+
+    }
+}
