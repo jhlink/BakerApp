70a4a9f0551d07dfa666e511f3eb78f36d31a6af
jhlink <jhlink@users.noreply.github.com>
2018-11-10 11:24:15 -0500

    feat: Implement skeleton for SelectRecipeDetails RV Adapter

--- app/src/main/java/com/example/oliverh/bakerapp/ui/selectstep/SelectRecipeDetailsRecyclerViewAdapter.java
+++ app/src/main/java/com/example/oliverh/bakerapp/ui/selectstep/SelectRecipeDetailsRecyclerViewAdapter.java
@@ -6,22 +6,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.oliverh.bakerapp.R;
 import com.example.oliverh.bakerapp.ui.selectstep.SelectRecipeDetailsFragment.OnListFragmentInteractionListener;
-import com.example.oliverh.bakerapp.ui.selectstep.dummy.DummyContent.DummyItem;
 
 import java.util.List;
 
 /**
- * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
+ * {@link RecyclerView.Adapter} that can display a generic text description and makes a call to the
  * specified {@link OnListFragmentInteractionListener}.
  * TODO: Replace the implementation with code for your data type.
  */
 public class SelectRecipeDetailsRecyclerViewAdapter extends RecyclerView.Adapter<SelectRecipeDetailsRecyclerViewAdapter.ViewHolder> {
 
-    private final List<DummyItem> mValues;
+    private final List<?> mValues;
     private final OnListFragmentInteractionListener mListener;
+    private static final int INGREDIENTS_VIEW_TYPE = 1;
+    private static final int STEPS_VIEW_TYPE = 2;
 
-    public SelectRecipeDetailsRecyclerViewAdapter(List<DummyItem> items, OnListFragmentInteractionListener listener) {
+    public SelectRecipeDetailsRecyclerViewAdapter(List<?> items, OnListFragmentInteractionListener listener) {
         mValues = items;
         mListener = listener;
     }
@@ -29,26 +31,23 @@ public class SelectRecipeDetailsRecyclerViewAdapter extends RecyclerView.Adapter
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.fragment_selectrecipedetails, parent, false);
+                .inflate(R.layout.recipe_details_list_item, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
-        holder.mItem = mValues.get(position);
-        holder.mIdView.setText(mValues.get(position).id);
-        holder.mContentView.setText(mValues.get(position).content);
+        String content = "";
+        switch ( position ) {
+            case 0:
+                // Pass 'Recipe Ingredients' static text to ViewHolder
+                break;
 
-        holder.mView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (null != mListener) {
-                    // Notify the active callbacks interface (the activity, if the
-                    // fragment is attached to one) that an item has been selected.
-                    mListener.onListFragmentInteraction(holder.mItem);
-                }
-            }
-        });
+            default:
+                // Pass 'Recipe Steps' text to ViewHolder.
+                break;
+        }
+        holder.bindString(content);
     }
 
     @Override
@@ -58,20 +57,16 @@ public class SelectRecipeDetailsRecyclerViewAdapter extends RecyclerView.Adapter
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
-        public final TextView mIdView;
         public final TextView mContentView;
-        public DummyItem mItem;
 
         public ViewHolder(View view) {
             super(view);
             mView = view;
-            mIdView = (TextView) view.findViewById(R.id.item_number);
             mContentView = (TextView) view.findViewById(R.id.content);
         }
 
-        @Override
-        public String toString() {
-            return super.toString() + " '" + mContentView.getText() + "'";
+        public void bindString(String content) {
+
         }
     }
 }
