99111499018f11d9186dc04538cbfeef9051e282
jhlink <jhlink@users.noreply.github.com>
2018-11-10 15:34:26 -0500

    feat: Implement basic ViewRecipeStepViewModel

---
+++ app/src/main/java/com/example/oliverh/bakerapp/ui/viewstep/ViewRecipeStepViewModel.java
@@ -0,0 +1,23 @@
+package com.example.oliverh.bakerapp.ui.viewstep;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.example.oliverh.bakerapp.data.RecipeRepository;
+import com.example.oliverh.bakerapp.data.database.RecipeStep;
+
+public class ViewRecipeStepViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+    private RecipeRepository recipeRepository;
+    private LiveData<RecipeStep> recipeStepLiveData;
+    private int recipeId;
+
+    public ViewRecipeStepViewModel(int recipeId) {
+        recipeRepository = RecipeRepository.getExistingInstance();
+        recipeStepLiveData = recipeRepository.getRecipeStep(recipeId, stepId);
+    }
+
+    public LiveData<RecipeStep> getRecipeStep() {
+        return recipeStepLiveData;
+    }
+}
