510c0e40802db44cc4cfc7dfc27283a0f7f62cea
jhlink <jhlink@users.noreply.github.com>
2018-11-10 18:47:26 -0500

    feat: Implement initializePlayer and buildMediaSource for ViewRecipeStepFragment

--- app/src/main/java/com/example/oliverh/bakerapp/ui/viewstep/ViewRecipeStepFragment.java
+++ app/src/main/java/com/example/oliverh/bakerapp/ui/viewstep/ViewRecipeStepFragment.java
@@ -2,10 +2,14 @@ package com.example.oliverh.bakerapp.ui.viewstep;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,15 +17,29 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.oliverh.bakerapp.R;
-import com.example.oliverh.bakerapp.data.database.Recipe;
 import com.example.oliverh.bakerapp.data.database.RecipeStep;
 import com.example.oliverh.bakerapp.data.network.RepositoryResponse;
+import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.source.dash.DashChunkSource;
+import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -31,6 +49,10 @@ public class ViewRecipeStepFragment extends Fragment implements ExoPlayer.EventL
 
     private static final String ARGS_RECIPE_ID = "RECIPE_ID";
     private static final String ARGS_STEP_ID = "STEP_ID";
+    private static final String TAG = ViewRecipeStepFragment.class.getSimpleName();
+    private static final DefaultBandwidthMeter BANDWIDTH_METER =
+            new DefaultBandwidthMeter();
+    private SimpleExoPlayer mExoPlayer;
     private ViewRecipeStepViewModel mViewModel;
     private int mRecipeId;
     private int mStepId;
@@ -67,7 +89,7 @@ public class ViewRecipeStepFragment extends Fragment implements ExoPlayer.EventL
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.view_recipe_step_activity, container, false);
+        final View view = inflater.inflate(R.layout.view_recipe_step_activity, container, false);
 
         ButterKnife.bind(this, view);
 
@@ -81,8 +103,10 @@ public class ViewRecipeStepFragment extends Fragment implements ExoPlayer.EventL
                 RecipeStep recipeStep = (RecipeStep) repositoryResponse.getObject();
                 recipeStepHeader.setText(String.format("Step %d", recipeStep.getStepIndex() + 1));
                 recipeStepDescription.setText(recipeStep.getDescription());
+                initializePlayer(view.getContext(), recipeStep.getVideoUrl());
             }
         });
+
 
         return view;
     }
@@ -94,6 +118,43 @@ public class ViewRecipeStepFragment extends Fragment implements ExoPlayer.EventL
 
     public void setStepId(int id) {
         this.mStepId = id;
+    }
+
+    private void initializePlayer(Context context, String mediaUri) {
+        if (mExoPlayer == null) {
+            // Create an instance of the ExoPlayer.
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(context, trackSelector, loadControl);
+            playerView.setPlayer(mExoPlayer);
+
+            // Set the ExoPlayer.EventListener to this activity.
+            mExoPlayer.addListener(this);
+
+            // Prepare the MediaSource.
+            String userAgent = Util.getUserAgent(context, "BakerApp");
+            Uri uri = Uri.parse(mediaUri);
+            MediaSource mediaSource = buildMediaSource(uri);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        DefaultExtractorsFactory extractorsFactory =
+                new DefaultExtractorsFactory();
+        DataSource.Factory manifestDataSourceFactory =
+                new DefaultHttpDataSourceFactory("ua");
+
+        DashChunkSource.Factory dashChunkSourceFactory =
+                new DefaultDashChunkSource.Factory(
+                        new DefaultHttpDataSourceFactory("ua", BANDWIDTH_METER));
+
+        ExtractorMediaSource videoSource =
+                new ExtractorMediaSource(uri, manifestDataSourceFactory, extractorsFactory,
+                        null, null);
+
+        return videoSource;
     }
 
     @Override
@@ -125,4 +186,5 @@ public class ViewRecipeStepFragment extends Fragment implements ExoPlayer.EventL
     public void onPositionDiscontinuity() {
 
     }
+
 }
