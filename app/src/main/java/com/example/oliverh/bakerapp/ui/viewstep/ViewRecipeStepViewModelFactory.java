e52ccd990f25505d874bc80f7158db05b5bfeee5
jhlink <jhlink@users.noreply.github.com>
2018-11-11 15:14:07 -0500

    fix: Correct ViewRecipeStep ViewModel Factory class name

--- app/src/main/java/com/example/oliverh/bakerapp/ui/viewstep/ViewRecipeStepViewModelFactory.java
+++ app/src/main/java/com/example/oliverh/bakerapp/ui/viewstep/ViewRecipeStepViewModelFactory.java
@@ -3,13 +3,11 @@ package com.example.oliverh.bakerapp.ui.viewstep;
 import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProvider;
 
-import com.example.oliverh.bakerapp.ui.selectstep.SelectRecipeDetailsFragmentViewModel;
-
-public class ViewRecipeStepFragmentViewModelFactory extends ViewModelProvider.NewInstanceFactory {
+public class ViewRecipeStepViewModelFactory extends ViewModelProvider.NewInstanceFactory {
     private final int mRecipeId;
     private final int mStepId;
 
-    public ViewRecipeStepFragmentViewModelFactory(int recipeId, int stepId) {
+    public ViewRecipeStepViewModelFactory(int recipeId, int stepId) {
         mRecipeId = recipeId;
         mStepId = stepId;
     }
